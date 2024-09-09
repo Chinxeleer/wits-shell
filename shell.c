39f35cf344a6c666a5078e9eebccd19afe631fc5
chinxeleer <blessingeleer.programming@gmail.com>
2024-09-09 21:25:00 +0200

    feature: execvp running

--- shell.c
+++ shell.c
@@ -2,59 +2,135 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <time.h>
 #include <unistd.h>
 
-void inputValidation() {
+#define MAX_TOKENS 64
+
+int built_ins(char **vec) {
+  if (strcmp(vec[0], "exit\n") == 0) {
+    free(vec);
+    exit(0);
+  } else if (strcmp(vec[0], "cd\n")) {
+    // change directory chdir
+  }
+
+  return 1;
+}
+
+char *get_input() {
   char *input = NULL;
   size_t len = 0;
 
-  printf("witsshell> ");
+  if (getline(&input, &len, stdin) == -1) {
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS); // We recieved an EOF
+    } else {
+      perror("readline");
+      exit(EXIT_FAILURE);
+    }
+  }
 
-  fflush(stdout);
-  ssize_t nread = getline(&input, &len, stdin);
+  return input;
+}
 
-  if (nread == -1) {
-    perror("getline");
+int exevute(char **args, char **path) {
+  pid_t pid;
+  int status;
+
+  pid = fork();
+
+  if (pid == 0) {
+    // Child process
+
+    for (int i = 0; i < 2; i++) {
+      perror("error executing");
+      if (execvp(path[i], args) == -1) {
+        perror("error executing");
+      }
+    }
+    exit(EXIT_FAILURE);
+  } else {
+    // Parent process
+    do {
+      waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+  return 1;
+}
+
+char **split_input(char *line) {
+  char **tokens = malloc(MAX_TOKENS * sizeof(char *));
+  char *token;
+  int position = 0;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
-  if (strcmp(input, "exit\n") == 0) {
-    free(input);
-  }
+  // Use strsep to tokenize the input based on space (" ")
+  while ((token = strsep(&line, " ")) != NULL) {
+    if (*token == '\0') { // Skip empty tokens
+      continue;
+    }
+    tokens[position] = token;
+    position++;
 
-  printf("input ----> %s", input);
+    if (position >= MAX_TOKENS - 1) {
+      break; // Stop if we've reached the maximum number of tokens
+    }
+  }
+  tokens[position] = NULL; // Null-terminate the array
+  return tokens;
 }
 
-void batchFileInput() {}
-void splitInputs() {}
+int batch_mode(int argc, char *argv[]) {
+  if (argc == 2) {
+
+    printf("the batch file is : %s \n", argv[1]);
+    return 0;
+
+  } else if (argc > 2) {
+
+    printf("Throw an error because we only require one input\n");
+    return 0;
+  }
+  return 0;
+}
+
+void interactive_mode() {
+  char *input;
+  char **args;
+  int status = 1;
+
+  char *path_list[] = {
+      "/bin/",
+      "/usr/bin/",
+      "/usr/local/bin/",
+  };
+
+  while (status) {
+    printf("witsshell> ");
+    input = get_input();
+    args = split_input(input);
+    status = exevute(args, path_list);
+
+    for (int i = 0; args[i] != NULL; i++) {
+      printf("args[%d] = %s\n", i, args[i]);
+    }
+
+    free(args);
+    free(input);
+  }
+}
+
+/* =============================== main ===================================*/
 
 int main(int argc, char *argv[]) {
 
-  int pid = fork();
+  batch_mode(argc, argv);
+  interactive_mode();
 
-  if (pid == 0) {
-
-    if (argc == 2) {
-
-      printf("the batch file is : %s \n", argv[1]);
-      return 0;
-
-    } else if (argc > 2) {
-
-      printf("Throw an error because we only require one input");
-      return 0;
-    }
-
-    /* strep --> to parse the input line into constituent pieces */
-
-    while (1) {
-      inputValidation();
-    }
-  }
-
-  else {
-    wait(NULL);
-    printf("exiting the programme...");
-  }
   return 0;
 }
